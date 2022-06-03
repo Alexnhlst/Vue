2550acde729617d98637ed6c582c3f2c7b193c72
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-03 18:58:50 +0200

    Created a single-file component

---
+++ src/app/MyComponent.vue
@@ -0,0 +1,34 @@
+<!-- A singe-file component focus heavily on coupling logic -->
+<!-- It consists of three parts: -->
+<!-- <template> - contains the component's markup in plain HTML -->
+<!-- <script> - exports the component object constructor that consists of all the JS logic -->
+<!-- <style> - contains all the component styles -->
+<template>
+    <h2>{{ getGreeting }}</h2>
+    <p>This is the Hello World component.</p>
+</template>
+
+<script>
+export default {
+    name: "MyComponent",
+    data() {
+        return {
+            reversedGreeting: "!dlrow olleH",
+        };
+    },
+    computed: {
+        getGreeting() {
+            return this.reversedGreeting.split("").reverse().join("")
+        }
+    }
+};
+</script>
+
+<!-- scoped dictates these styles will be applied to this and only this component -->
+<style lang="scss" scoped >
+h2,
+p {
+    width: 100%;
+    text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // Importing createApp
 import { createApp } from "vue";
 // Importing the main app
-import App from "./app/App.vue";
+// import App from "./app/App.vue";
+// Importing a single-file component
+import MyComponent from "./app/MyComponent.vue";
 
 // Creating the app
-createApp(App).mount("#app");
+createApp(MyComponent).mount("#app");
