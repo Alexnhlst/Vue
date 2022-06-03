06386426ae192156d221b80037092e89d59623d0
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-03 20:04:06 +0200

    Created components and moved HTML and scss from app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 // Importing createApp
 import { createApp } from "vue";
 // Importing the main app
-// import App from "./app/App.vue";
+import App from "./app/App.vue";
 // Importing a single-file component
 // import MyComponent from "./app/MyComponent.vue";
 // Importing components wich share a store
-import NumberDisplay from "./app/NumberDisplay.vue";
-import NumberSubmit from "./app/NumberSubmit.vue";
+// import NumberDisplay from "./app/NumberDisplay.vue";
+// import NumberSubmit from "./app/NumberSubmit.vue";
 
 // Creating the app
-createApp(NumberDisplay).mount("#app");
-createApp(NumberSubmit).mount("#app2");
+createApp(App).mount("#app");
