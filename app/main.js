0eeb1213277a9b5e7e5868e4a62628a02f57c5f4
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-04 16:00:26 +0200

    Added Vuex mutations

--- app/main.js
+++ app/main.js
@@ -19,6 +19,29 @@ const state = {
   timestamps: [],
 };
 
+// during the lifetime of the application we need to push a new note to the state notes array
+// and push a new timestamp to the state timestamps array
+// mutation are functions responsible to mutate the store state
+// in Vuex, mutations need to be explicitly defined
+// it consists of a string type and a handler
+// in flux architectures, mutation string types are often declared in capital letters
+// when the mutation is run, the first argument passed is the state
+// mutations always have access to state as the first argument
+// in addition, when an action calls a mutation, it may or may not pass a payload to a mutation
+// the payload is an optional argument and, in some cases we can safely ignore it
+// the ability to always have access to state arises from how a Vuex store is wired together
+// the payload exists only when an action subsequently passes it to the mutation
+const mutations = {
+  ADD_NOTE(state, payload) {
+    let newNote = payload;
+    state.notes.push(newNote);
+  },
+  ADD_TIMESTAMP(state, payload) {
+    let newTimeStamp = payload;
+    state.timestamps.push(newTimeStamp);
+  },
+};
+
 const inputComponent = {
   template: `<input placeholder="Enter a note" class="input is-small" type="text"/>`,
 };
