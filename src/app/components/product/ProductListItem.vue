7f2d530506ae17e2e8c903827e3af82e31dc650f
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-04 22:41:30 +0200

    Configured addCartItem

--- src/app/components/product/ProductListItem.vue
+++ src/app/components/product/ProductListItem.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h2 class="has-text-weight-bold">{{ productItem.title }}
-            <span class="tag is-primary is-pulled-right has-text-white">Add to Cart</span>
+            <span @click="addCartItem(productItem)" class="tag is-primary is-pulled-right has-text-white">
+                Add to Cart
+            </span>
         </h2>
         <p>{{ productItem.description }}</p>
         <span class="has-text-primary has-text-weight-bold">
@@ -12,9 +14,16 @@
 </template>
 
 <script>
+// mapAction is an helper method that directly maps the component method action to the store action
+import { mapActions } from 'vuex'
 export default {
     name: "ProductListItem",
-    props: ["productItem"]
+    props: ["productItem"],
+    methods: {
+        ...mapActions([
+            "addCartItem"
+        ])
+    }
 }
 </script>
 
