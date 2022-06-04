aab9de08ac87695acb207fa14c626fd18b893923
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-04 15:22:26 +0200

    Added Vuex introduction section

---
+++ app/main.js
@@ -0,0 +1,25 @@
+// The Vuex store is where the application data (state) is kept
+// State can never be mutated directly and can only be modified by calling mutations
+// Actions are often responsible in calling mutations an are themselves dispatched within components
+// A Vuex store also allows to define getters
+
+// Vuex's key ideas:
+// - all of the application's data is in a single data structure called the state, which is held in the store
+// - the apps reads the state from this store
+// - the state is never mutated directly outside the store
+// - the views dispatch actions that describe what happened
+// - the actions commit to mutations
+// - mutations directly mutate/change store state
+// - when the state is mutated, relevant components/views are re-rendered
+
+const inputComponent = {
+  template: `<input placeholder="Enter a note" class="input is-small" type="text"/>`,
+};
+
+const app = {
+  components: {
+    "input-component": inputComponent,
+  },
+};
+
+Vue.createApp(app).mount("#app");
