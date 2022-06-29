41186fc03e1f8d7c180d92a8b7a80be1752f39bc
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-29 20:22:06 +0200

    Created branch for chapter 8

--- shopping_cart/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./app/App.vue";
-import store from "./app/store";
-import router from "./app/router";
-
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
