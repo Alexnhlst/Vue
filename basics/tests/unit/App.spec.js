df0a6fd2389044ce2258a66a2705864f8c0e965c
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-30 18:47:29 +0200

    Added tests for data input and input clear

--- basics/tests/unit/App.spec.js
+++ basics/tests/unit/App.spec.js
@@ -2,24 +2,6 @@ import App from "@/App";
 import { shallowMount } from "@vue/test-utils";
 import { expect } from "chai";
 
-/*
-In vue 2 it was simpler to test if a component is mounted
-describe("App.vue", () => {
-  it("should render correct contents", () => {
-    const Constructor = Vue.extend(App);
-    const vm = new Constructor().$mount();
-    // CSS selectors were used to check the assertion
-    expect(
-      vm.$el.querySelector(".ui.selectable thead tr th").textContent
-    ).to.contain("Items");
-    expect(vm.$el.querySelector(".ui.button").textContent).to.contain("Add");
-    expect(vm.$el.querySelector(".ui.label").textContent).to.contain(
-      "Remove all"
-    );
-  });
-});
-*/
-
 describe("App.vue", () => {
   // A wrapper is an object that contains a mounted component and the accompanying methods to help test the component
   // shallowMount() allows to mount a component without rendering its children
@@ -30,25 +12,60 @@ describe("App.vue", () => {
   beforeEach(() => {
     wrapper = shallowMount(App);
   });
-  it(
-    "should render correct contents",
-    () => {
-      // The html() helper method helps retrieve a component's HTML
-      expect(wrapper.html()).to.contain("<th>Items</th>");
-      expect(wrapper.html()).to.contain(
-        '<input type="text" class="prompt" placeholder="Add item...">'
-      );
-      expect(wrapper.html()).to.contain(
-        '<button type="submit" class="ui button" disabled="">Add</button>'
-      );
-      expect(wrapper.html()).to.contain(
-        '<span class="ui label">Remove all</span>'
-      );
-    },
-    it("should set correct default data", () => {
-      // vm is used to access the Vue instance
-      expect(wrapper.vm.item).to.equal("");
-      expect(wrapper.vm.items).to.deep.equal([]);
-    })
-  );
+
+  it("should render correct contents", () => {
+    // The html() helper method helps retrieve a component's HTML
+    expect(wrapper.html()).to.contain("<th>Items</th>");
+    expect(wrapper.html()).to.contain(
+      '<input type="text" class="prompt" placeholder="Add item...">'
+    );
+    expect(wrapper.html()).to.contain(
+      '<button type="submit" class="ui button" disabled="">Add</button>'
+    );
+    expect(wrapper.html()).to.contain(
+      '<span class="ui label">Remove all</span>'
+    );
+  });
+
+  it("should set correct default data", () => {
+    // vm is used to access the Vue instance
+    expect(wrapper.vm.item).to.equal("");
+    expect(wrapper.vm.items).to.deep.equal([]);
+  });
+
+  it('should have the "Add" button diabled', () => {
+    const addItemButton = wrapper.find(".ui.button");
+    expect(addItemButton.element.disabled).to.be.true;
+  });
+
+  describe("the user populates the text input field", () => {
+    let inputField;
+    // Vue asynchronously updates the DOM based on changes to data
+    // test runners like Mocha run synchronously
+    beforeEach(async () => {
+      inputField = wrapper.find("input");
+      inputField.element.value = "New Item";
+      // the trigger() method simulates the user interaction
+      // it accepts two arguments: the event to simulate, and an event object (optional)
+      await inputField.trigger("input");
+    });
+
+    it('should update the "text" data property', () => {
+      expect(wrapper.vm.item).to.equal("New Item");
+    });
+
+    it('should enable the "Add" button when text input is populated', () => {
+      const addItemButton = wrapper.find(".ui.button");
+      expect(addItemButton.element.disabled).to.be.false;
+    });
+
+    describe("and then clears the input", () => {
+      it('should disable the "Add" button', async () => {
+        const addItemButton = wrapper.find(".ui.button");
+        inputField.element.value = "";
+        await inputField.trigger("input");
+        expect(addItemButton.element.disabled).to.be.true;
+      });
+    });
+  });
 });
