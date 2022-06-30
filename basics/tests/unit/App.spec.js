5e7bc7843b87c130c2d44ac595aaeb209c35ab15
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-06-30 17:50:03 +0200

    Implemented Vue 2 testing

--- basics/tests/unit/App.spec.js
+++ basics/tests/unit/App.spec.js
@@ -8,3 +8,19 @@ describe("App.vue", () => {
     expect(initialData.items).to.deep.equal([]);
   });
 });
+
+// In vue 2 it was simpler to test if a component is mounted
+describe("App.vue", () => {
+  it("should render correct contents", () => {
+    const Constructor = Vue.extend(App);
+    const vm = new Constructor().$mount();
+    // CSS selectors were used to check the assertion
+    expect(
+      vm.$el.querySelector(".ui.selectable thead tr th").textContent
+    ).to.contain("Items");
+    expect(vm.$el.querySelector(".ui.button").textContent).to.contain("Add");
+    expect(vm.$el.querySelector(".ui.label").textContent).to.contain(
+      "Remove all"
+    );
+  });
+});
