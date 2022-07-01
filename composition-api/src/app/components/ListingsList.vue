b0f2457c484207862cab2bf8f2963a4f3894a71c
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-01 19:16:48 +0200

    Refactored ListingList component with setup()

--- composition-api/src/app/components/ListingsList.vue
+++ composition-api/src/app/components/ListingsList.vue
@@ -4,10 +4,7 @@
     <div v-for="listing in listings" :key="listing.id">
       <ListingsListItem :listing="listing" :isDark="isDark" />
     </div>
-    <button
-      class="button is-light"
-      :class="{ 'is-primary': isDark, 'is-info': !isDark }"
-      @click="resetListings" 
+    <button class="button is-light" :class="{ 'is-primary': isDark, 'is-info': !isDark }" @click="resetListings"
       :disabled="listings.length === 3">
       Reset
     </button>
@@ -15,6 +12,7 @@
 </template>
 
 <script>
+/* Original
 import { mapActions } from 'vuex';
 import ListingsListItem from './ListingsListItem';
 import Notification from './Notification';
@@ -42,6 +40,40 @@ export default {
     setTimeout(() => {
       this.notification = null;
     }, 1000);
+  }
+}
+*/
+// Refactored
+import { ref, onMounted } from "vue"
+import { useStore } from "vuex"
+import ListingsListItem from './ListingsListItem.vue'
+import Notification from './Notification.vue'
+export default {
+  name: "ListingsList",
+  props: ["listings", "isDark"],
+  setup() {
+    // Accessing the store
+    const store = useStore();
+    // Reactive data properties
+    const notification = ref(null);
+    // Methods
+    const resetListings = () => store.dispatch("resetListings");
+    // Mounted lifecycle hook
+    onMounted(() => {
+      notification.value = "Welcome to NewlineBnB!"
+      setTimeout(() => {
+        notification.value = null
+      }, 1000);
+    });
+    // Returning properties for component to access
+    return {
+      notification,
+      resetListings
+    }
+  },
+  components: {
+    ListingsListItem,
+    Notification
   }
 }
 </script>
