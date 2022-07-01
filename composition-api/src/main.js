2dc29b00215bf1d7d71e2c56ce6db5409d6242a8
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-01 20:46:40 +0200

    Implemented provide

--- composition-api/src/main.js
+++ composition-api/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./app/App.vue";
 import store from "./app/store";
 
+// provide/inject pattern relies on the parent component to provide data to any child component
+// child components can access the provided value with the inject option
+// the child component must be a direct descendant of the parent component
 createApp(App)
-  .use(store)
+  .provide("store", store)
   .mount("#app");
